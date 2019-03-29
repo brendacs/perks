9316e989259b4242f3cd6e44f18ab00a956df2c4
Brenda Zhang <brenda.zhang2@gmail.com>
2019-03-29 00:53:08 -0700

    Fix guide container positioning

--- src/components/Guide/styles.js
+++ src/components/Guide/styles.js
@@ -29,7 +29,7 @@ export const GuideContainer = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: rgba(0, 0, 0, 0.7);
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   display: flex;
