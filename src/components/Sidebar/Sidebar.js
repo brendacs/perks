91173942f4686d5e3befe6d7df9e3dbc1286dfdb
Daniel Mao <daniel.mao@gusto.com>
2022-03-08 16:37:41 -0800

    Convert Sidebar class to functional component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,46 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container, Section, Heading, LinkContainer, Link } from './styles';
 import perks from '../../constants/perks.json';
 
-class Sidebar extends Component {
-  render() {
-    const companyPerks = perks["google"];
-    const perkList = Object.keys(perks["google"]);
-    const companyList = Object.keys(perks);
+const Sidebar = ({ addCard, comparePerk }) => {
+  const companyPerks = perks["google"];
+  const perkList = Object.keys(perks["google"]);
+  const companyList = Object.keys(perks);
 
-    return (
-      <Container>
-        <Section>
-          <Heading>Companies</Heading>
-          <LinkContainer>
-            {
-              companyList.sort().map((item, idx) => {
-                return (
-                  <Link onClick={() => this.props.addCard(idx)}>
-                    {item.charAt(0).toUpperCase() + item.substring(1)}
-                  </Link>
-                );
-              })
-            }
-          </LinkContainer>
-        </Section>
-        <Section>
-          <Heading>Perks</Heading>
-          <LinkContainer>
-            {
-              perkList.map((item, idx) => {
-                return (
-                  <Link onClick={() => this.props.comparePerk(idx)}>
-                    {companyPerks[item].category}
-                  </Link>
-                );
-              })
-            }
-          </LinkContainer>
-        </Section>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Section>
+        <Heading>Companies</Heading>
+        <LinkContainer>
+          {
+            companyList.sort().map((item, idx) => {
+              return (
+                <Link onClick={() => addCard(idx)}>
+                  {item.charAt(0).toUpperCase() + item.substring(1)}
+                </Link>
+              );
+            })
+          }
+        </LinkContainer>
+      </Section>
+      <Section>
+        <Heading>Perks</Heading>
+        <LinkContainer>
+          {
+            perkList.map((item, idx) => {
+              return (
+                <Link onClick={() => comparePerk(idx)}>
+                  {companyPerks[item].category}
+                </Link>
+              );
+            })
+          }
+        </LinkContainer>
+      </Section>
+    </Container>
+  );
 }
 
 export default Sidebar;
